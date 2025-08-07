464fd8ade3a307fbea03b6672c97ce86065dea39
M C RAM PRABHAS <Prabhasram763@gmail.com>
2025-08-07 09:42:01 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,8 @@
+# initializing empty set
+s1 = set()
+
+s1 = set("villain")
+print("Set with the use of String: ", s1)
+
+s2 = set(["azma", "bebo", "zuzu"])
+print("Set with the use of List: ", s2)
---
+++ stud.py
@@ -0,0 +1,5 @@
+arr=[275,278,504]
+arr.pop(2)
+print(arr)
+arr.clear(2)
+print(arr)
